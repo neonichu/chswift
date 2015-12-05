ad1d31143528a30da8c0b9a595760bc015494142
Boris Bügling <boris@icculus.org>
2015-12-05 11:00:28 +0100

    Update tests

--- test/helper.sh
+++ test/helper.sh
@@ -5,9 +5,9 @@ export PREFIX="$PWD/test"
 export HOME="$PREFIX/home"
 export PATH="$PWD/bin:$PATH"
 
-export LIST=" * 1.1 (swift-600.0.57.4)    1.2 (swiftlang-602.0.47.4 clang-602.0.48) "
-export VERSION_1_1="Swift version 1.1 (swift-600.0.57.4)"
-export VERSION_1_2="Apple Swift version 1.2 (swiftlang-602.0.47.4 clang-602.0.48)"
+export LIST=" * 2.1.1 (swiftlang-700.1.101.13 clang-700.1.81)\n   1.2 (swiftlang-602.0.53.1 clang-602.0.53)\n   2.1 (swiftlang-700.1.101.6 clang-700.1.76)\n"
+export VERSION_1_1="Apple Swift version 2.1.1 (swiftlang-700.1.101.13 clang-700.1.81)"
+export VERSION_1_2="Apple Swift version 1.2 (swiftlang-602.0.53.1 clang-602.0.53)"
 
 . ./share/chswift/chswift.sh
 chswift_reset
--- test/list_test.sh
+++ test/list_test.sh
@@ -5,14 +5,15 @@ function setUp() {
 }
 
 function test_chswift_list() {
-	output=$(chswift|tr '\n' ' ')
-	assertEquals "wrong list" "$LIST" "$output"
+	output=$(chswift)
+	assertEquals "wrong list" "`echo -e "$LIST"`" "$output"
 }
 
 function test_chswift_list_robustness() {
 	export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer/"
-	output=$(chswift|tr '\n' ' ')
-	assertEquals "wrong list" "$LIST" "$output"
+	expected=$(echo -e "$LIST"|tr '*' ' '|sort -n|tr -d '\n')
+	output=$(chswift|tr -d '\n')
+	assertEquals "wrong list" "$expected" "$output"
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
