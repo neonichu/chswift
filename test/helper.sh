36ab6bde4cf457c4e9569870e8022adf5ffeea46
Boris Bügling <boris@icculus.org>
2016-01-09 18:45:33 +0100

    Update tests

--- test/helper.sh
+++ test/helper.sh
@@ -5,11 +5,11 @@ export PREFIX="$PWD/test"
 export HOME="$PREFIX/home"
 export PATH="$PWD/bin:$PATH"
 
-export LIST=" * 2.1.1 (swiftlang-700.1.101.13 clang-700.1.81)\n   1.2 (swiftlang-602.0.53.1 clang-602.0.53)\n   2.1 (swiftlang-700.1.101.6 clang-700.1.76)\n   2.2-dev (LLVM 46be9ff861, Clang 4deb154edc, Swift 778f82939c)\n"
-export VERSION_1_1="Apple Swift version 2.1.1 (swiftlang-700.1.101.13 clang-700.1.81)"
+export LIST=" * 2.1.1 (swiftlang-700.1.101.15 clang-700.1.81)\n   1.2 (swiftlang-602.0.53.1 clang-602.0.53)\n   2.1 (swiftlang-700.1.101.6 clang-700.1.76)\n   2.2-dev (LLVM 3ebdbb2c7e, Clang f66c5bb67b, Swift 17fe37d715)\n"
+export VERSION_1_1="Apple Swift version 2.1.1 (swiftlang-700.1.101.15 clang-700.1.81)"
 export VERSION_1_2="Apple Swift version 1.2 (swiftlang-602.0.53.1 clang-602.0.53)"
-export VERSION_DEV="Apple Swift version 2.2-dev (LLVM 46be9ff861, Clang 4deb154edc, Swift 778f82939c)"
-export TOOLCHAINS="/Library/Developer/Toolchains/swift-2.2-SNAPSHOT-2015-12-01-a.xctoolchain/usr/bin"
+export VERSION_DEV="Apple Swift version 2.2-dev (LLVM 3ebdbb2c7e, Clang f66c5bb67b, Swift 17fe37d715)"
+export TOOLCHAINS="/Library/Developer/Toolchains/swift-2.2-SNAPSHOT-2015-12-22-a.xctoolchain/usr/bin"
 
 . ./share/chswift/chswift.sh
 chswift_reset
--- test/list_test.sh
+++ test/list_test.sh
@@ -9,14 +9,15 @@ function test_chswift_find_toolchains() {
 }
 
 function test_chswift_list() {
-	output=$(chswift)
-	assertEquals "wrong list" "`echo -e "$LIST"`" "$output"
+	expected=$(echo -e "$LIST"|tr '*' ' '|sort -n|tr -d '\n')
+	output=$(chswift|tr '*' ' '|sort -n|tr -d '\n')
+	assertEquals "wrong list" "$expected" "$output"
 }
 
 function test_chswift_list_robustness() {
 	export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer/"
 	expected=$(echo -e "$LIST"|tr '*' ' '|sort -n|tr -d '\n')
-	output=$(chswift|tr -d '\n')
+	output=$(chswift|tr '*' ' '|sort -n|tr -d '\n')
 	assertEquals "wrong list" "$expected" "$output"
 }
 
